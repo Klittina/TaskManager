f81cd2bc05c2c08b81dd7f3557f831fbae4d2dad
Klittina <krisztike113@gmail.com>
2024-09-08 18:50:24 +0200

    Kiindulási alap

---
+++ app/Models/StepsTask.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StepsTask extends Model
+{
+    use HasFactory;
+}
